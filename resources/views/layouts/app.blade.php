6d69dbea6efa2413fd580232c99e443ab8e8dd1b
akramulfata10 <superlay582@gmail.com>
2023-02-23 16:51:49 +0700

    install liveware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+	@livewireStyles
+	@livewareScripts
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
