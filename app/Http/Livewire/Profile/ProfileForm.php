878617f1b7d4616ff763e453003bfa596892d058
akramulfata10 <superlay582@gmail.com>
2023-02-27 02:58:20 +0700

    create liveware Inline component

---
+++ app/Http/Livewire/Profile/ProfileForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire\Profile;
+
+use Livewire\Component;
+
+class ProfileForm extends Component {
+    public function render(): string {
+        return <<<'blade'
+            <div>
+                hello
+            </div>
+        blade;
+    }
+}
