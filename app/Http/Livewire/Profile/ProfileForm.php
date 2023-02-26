3715a44b15b74f52dc21969fe0587b5a561a1fe4
akramulfata10 <superlay582@gmail.com>
2023-02-27 03:26:51 +0700

    implements<button

--- app/Http/Livewire/Profile/ProfileForm.php
+++ app/Http/Livewire/Profile/ProfileForm.php
@@ -1,14 +1,38 @@
 <?php
 
+declare (strict_types = 1);
+
 namespace App\Http\Livewire\Profile;
 
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Livewire\Component;
 
-class ProfileForm extends Component {
+class ProfileForm extends Component implements HasForms {
+
+    use InteractsWithForms;
+
+    protected function getFormSchema(): array
+    {
+        return [
+            MarkdownEditor::make('bio')->required(),
+        ];
+    }
+
     public function render(): string {
         return <<<'blade'
             <div>
-                hello
+                <form wire:submit.prevent="submit" class="mt-3">
+                    {{ $this->form }}
+
+                    <button
+                        type="submit"
+                        class="mt-2 bg-gray-800 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                    >
+                        Save
+                    </button>
+                </form>
             </div>
         blade;
     }
--- resources/views/app/profile/show.blade.php
+++ resources/views/app/profile/show.blade.php
@@ -1,17 +1,13 @@
 <x-app-layout>
     <section>
         <div class="max-w-3xl mx-auto py-10 px-4 sm:px-6 lg:py-12 lg:px-8 bg-white rounded-lg shadow-lg">
-
             <div class="space-y-6">
                 <div>
-                    <h1 class="text-lg leading-6 font-medium text-gray-900">
-                        Bio
-                    </h1>
                     <p class="mt-1 text-sm text-gray-500">
                         Get started by adding a bio about yourself.
                     </p>
+                    <livewire:profile.profile-form />
                 </div>
-                <livewire:profile.profile-form />
             </div>
         </div>
     </section>
