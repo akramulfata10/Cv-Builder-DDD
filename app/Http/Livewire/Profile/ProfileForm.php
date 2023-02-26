d54aaa24e33fe54ce86a9e9aa938c1cc7ded38d3
akramulfata10 <superlay582@gmail.com>
2023-02-27 03:27:57 +0700

    implements form laravel filament

--- app/Http/Livewire/Profile/ProfileForm.php
+++ app/Http/Livewire/Profile/ProfileForm.php
@@ -26,6 +26,8 @@ class ProfileForm extends Component implements HasForms {
                 <form wire:submit.prevent="submit" class="mt-3">
                     {{ $this->form }}
 
+                    coba implements form fillament
+
                     <button
                         type="submit"
                         class="mt-2 bg-gray-800 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
