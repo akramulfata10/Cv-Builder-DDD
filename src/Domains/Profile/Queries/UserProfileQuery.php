1f14209205d9feb7884f47379c796a65c96a6c62
akramul fata <superlay582@gmail.com>
2023-04-28 15:59:23 +0700

    registrasi profile providers

--- src/Domains/Profile/Queries/UserProfileQuery.php
+++ src/Domains/Profile/Queries/UserProfileQuery.php
@@ -4,7 +4,17 @@ declare (strict_types = 1);
 
 namespace Domains\Profile\Queries;
 
-class UserProfileQueries
-{
+use App\Models\Profile;
+use Illuminate\Database\Eloquent\Model;
+use Infratructure\Profile\Queries\UserProfileQueryContract;
 
+class UserProfileQueries implements UserProfileQueryContract
+{
+    public function handle(Model $user): Model
+    {
+        return Profile::query()->whereBelongsTo(
+            related:$user,
+            relationshipName:'owner',
+        );
+    }
 }
--- src/Infrastructure/Profile/Queries/UserProfileQueryContract.php
+++ src/Infrastructure/Profile/Queries/UserProfileQueryContract.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 
 namespace Infratructure\Profile\Queries;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 interface UserProfileQueryContract
@@ -12,5 +13,5 @@ interface UserProfileQueryContract
      * @param int $user
      * @return Model
      */
-    public function handle(int $user): Model;
+    public function handle(Authenticatable $user): Model;
 }
